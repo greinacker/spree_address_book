05ee3dc505c552a1a90921afe2fff177f42977a1
Greg Reinacker <gregr@rassoc.com>
2013-08-16 13:33:27 -0600

    change from current_user to spree_current_user

--- app/controllers/spree/addresses_controller.rb
+++ app/controllers/spree/addresses_controller.rb
@@ -24,7 +24,7 @@ class Spree::AddressesController < Spree::StoreController
   end
 
   def create
-    @address.user = current_user
+    @address.user = spree_current_user
     if @address.save
       flash[:notice] = I18n.t(:successfully_created, :resource => I18n.t(:address))
       redirect_to account_path
